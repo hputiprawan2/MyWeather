ba82562cebad326c119863b1a422fb9fbf7ecbc7
Hanna Putiprawan <hanna.p@apple.com>
2021-01-09 20:26:23 -0800

    [Update] WeatherTableViewCell.xib

--- MyWeather/ViewController.swift
+++ MyWeather/ViewController.swift
@@ -17,10 +17,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet var table: UITableView!
     
-    var models = [Weather]()
-    
+    var models = [DailyWeather]()
+
     let locationManager = CLLocationManager()
-    
     var currentLocation: CLLocation? // capture current coordinate
         
     override func viewDidLoad() {
@@ -42,17 +41,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(true)
+        super.viewDidAppear(animated)
         setupLocation()
     }
-
+    
     // Tables
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: WeatherTableViewCell.identifier, for: indexPath) as! WeatherTableViewCell
+        cell.config(with: models[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
     }
     
     // Location
@@ -71,26 +80,126 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func requestWeatherForLocation() {
+        
         // Make sure currentLocation is not nil
         guard let currentLocation = currentLocation else {
             return
         }
         let long = currentLocation.coordinate.longitude
         let lat = currentLocation.coordinate.latitude
-        let url = "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(long)&appid=575606feecc3aa1ca745bf14f73c9d99"
+        let url = "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(long)&exclude=alerts,minutely&appid=575606feecc3aa1ca745bf14f73c9d99"
         
-        URLSession.shared.dataTask(with: URL(string: url)!) { (data, response, error) in
+        URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { (data, response, error) in
             // Validation: get the data back with no error
             guard let data = data, error == nil else {
                 print("Something went wrong")
                 return
             }
             // Convert data to models/ some objects
+            var json: WeatherResponse?
+            do {
+                json = try JSONDecoder().decode(WeatherResponse.self, from: data)
+            }
+            catch {
+                print("Error: \(error)")
+            }
+            
+            guard let result = json else {
+                return
+            }
+            
             // Update UI
-        }
+            DispatchQueue.main.async {
+                self.table.reloadData()
+            }
+            
+        }).resume()
     }
+    
 }
 
-struct Weather {
-    
+
+// Codable convert JSON data to object; needs to match Json key and the actual return type
+
+struct WeatherResponse: Codable {
+    let lat: Double
+    let lon: Double
+    let timezone: String
+    let timezone_offset: Int
+    let current: CurrentWeather
+    let hourly: [HourlyWeather]
+    let daily: [DailyWeather]
+}
+
+struct CurrentWeather: Codable {
+    let dt: Int
+    let sunrise: Int
+    let sunset: Int
+    let temp: Float
+    let feels_like: Float
+    let pressure: Int
+    let humidity: Int
+    let dew_point: Float
+    let uvi: Float?
+    let clouds: Int
+    let visibility: Int
+    let wind_speed: Float
+    let wind_deg: Int
+    let weather: [Weather]
+}
+
+struct HourlyWeather: Codable {
+    let dt: Int
+    let temp: Float
+    let feels_like: Float
+    let pressure: Int
+    let humidity: Int
+    let dew_point: Float
+    let uvi: Float?
+    let clouds: Int
+    let visibility: Int
+    let wind_speed: Float
+    let wind_deg: Int
+    let weather: [Weather]
+    let pop: Float
+}
+
+struct DailyWeather: Codable {
+    let dt: Int
+    let sunrise: Int
+    let sunset: Int
+    let temp: Temp
+    let feels_like: FeelsLike
+    let pressure: Int
+    let humidity: Int
+    let dew_point: Float
+    let wind_speed: Float
+    let wind_deg: Int
+    let weather: [Weather]
+    let clouds: Int
+    let pop: Float
+    let uvi: Float?
+}
+
+struct Weather: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Temp: Codable {
+    let day: Float
+    let min: Float
+    let max: Float
+    let night: Float
+    let eve: Float
+    let morn: Float
+}
+
+struct FeelsLike: Codable {
+    let day: Float
+    let night: Float
+    let eve: Float
+    let morn: Float
 }
--- MyWeather/WeatherCell/WeatherTableViewCell.swift
+++ MyWeather/WeatherCell/WeatherTableViewCell.swift
@@ -11,19 +11,41 @@ class WeatherTableViewCell: UITableViewCell {
 
     static let identifier = "WeatherTableViewCell"
     
+    @IBOutlet var dayLabel: UILabel!
+    @IBOutlet var highTemp: UILabel!
+    @IBOutlet var lowTemp: UILabel!
+    @IBOutlet var iconImageView: UIImageView!
+    
     static func nib() -> UINib {
         return UINib(nibName: "WeatherTableViewCell", bundle: nil)
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func config(with model: DailyWeather) {
+        print(model.temp.min)
+        print(model.temp.max)
+        self.lowTemp.text = "\(Int(model.temp.min))°"
+        self.highTemp.text = "\(Int(model.temp.max))°"
+//        self.dayLabel.text = getDayForDate(Date(timeIntervalSince1970: Double(model.dt)))
+//        self.iconImageView.image = UIImage(named: "Sun")
+    }
+    
+    private func getDayForDate(_ date: Date?) -> String {
+        guard let inputDate = date else {
+            return ""
+        }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM" // Monday
+        return formatter.string(from: inputDate)
     }
     
 }
